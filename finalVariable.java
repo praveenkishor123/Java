5c0072ce43e520cd2deb237e2103f54f8b031762
Praveen Kishor <praveen.kishor123@gmail.com>
2016-11-28 03:02:52 +0530

    Update finalVariable.java

--- finalVariable.java
+++ finalVariable.java
@@ -1,5 +1,8 @@
 package coreJava;
 
+// In Java, when final keyword is used with a variable of primitive
+// data types (int, float, etc.), then the value of variable cannot be changed.
+
 public class finalVariable {
 
 	    public static void main(String args[]) {
