6986efe601934175ec705cc6d2ebe2d5cb8eb47e
Praveen Kishor <praveen.kishor123@gmail.com>
2016-11-28 03:01:00 +0530

    Create finalVariable.java

---
+++ finalVariable.java
@@ -0,0 +1,10 @@
+package coreJava;
+
+public class finalVariable {
+
+	    public static void main(String args[]) {
+	        final int i = 10;
+	        i = 30; // Error because i is final.
+	    }
+	
+}
