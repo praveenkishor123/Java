1a929b8d3cf0f72c42b1bceca7731d7319c46723
Praveen Kishor <praveen.kishor123@gmail.com>
2016-11-28 03:13:12 +0530

    Create InstanceVariableHiding.java

---
+++ InstanceVariableHiding.java
@@ -0,0 +1,30 @@
+package coreJava;
+
+class Test
+{
+    // Instance variable or member variable
+    private int value = 10; 
+ 
+    void method()
+    {
+        // This local variable hides instance variable
+        int value = 40;
+ 
+        System.out.println("Value of Instance variable :" + this.value);
+        System.out.println("Value of Local variable :" + value);
+    }
+}
+ 
+public class InstanceVariableHiding
+{
+    public static void main(String args[])
+    {
+        Test obj1 = new Test();
+        obj1.method();
+    }
+}
+//
+//Output
+//-------
+//Value of Instance variable :10
+//Value of Local variable :40
