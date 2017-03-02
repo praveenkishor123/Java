e64a0ca197e0ffe8ce631fb672a0b8d0eced0535
Praveen Kishor <praveen.kishor123@gmail.com>
2017-03-03 00:03:23 +0530

    Create Main4.java

---
+++ Main4.java
@@ -0,0 +1,27 @@
+
+class Main4 {
+	
+	public static void main(String[] args) {
+		
+		char a = 'a';
+		
+		char b = (char) -10;
+		
+		char c = '1';
+		
+		Integer d = 1000;
+		
+		System.out.println(++a); // b
+		
+		System.out.println(a++); // b
+		
+		System.out.println(b++); // ?
+		
+		System.out.println(++b); // ?
+		
+		System.out.println(c - d); // -951
+		
+		
+	}
+
+}
