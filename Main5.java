27d4c65917c850a9933be6ab6b99e6e244281a44
Praveen Kishor <praveen.kishor123@gmail.com>
2017-03-03 00:15:35 +0530

    Create Main5.java

---
+++ Main5.java
@@ -0,0 +1,16 @@
+
+public class Main5 {
+	
+	public static void main(String[] args) {
+		
+		int a = 10;
+		
+		long b = 20;
+		
+		short c = 30;
+		
+		System.out.println(++a + b++ * c);  // 611
+		
+	}
+
+}
