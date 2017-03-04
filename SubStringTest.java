1a81d65b6187e4ca404eb245d0c6d9f3ba1b67b5
Praveen Kishor <praveen.kishor123@gmail.com>
2017-03-05 02:42:34 +0530

    Create SubStringTest.java

---
+++ SubStringTest.java
@@ -0,0 +1,27 @@
+/**
+ * @author praveen.kishor
+ *
+ */
+class SubStringTest {
+
+	/**
+	 * @param args
+	 */
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		
+		String str = "Hello";
+		
+		String a = str.substring(2, 4);  // ll
+		
+		String b = str.substring(0, 3);  // Hel
+		
+		String c = str.substring(4, 5);  // o
+		
+		System.out.println(a);
+		System.out.println(b);
+		System.out.println(c);
+
+	}
+
+}
