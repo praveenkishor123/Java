d38ee5cc152f10c317a57a8495d935775cfc11e0
Praveen Kishor <praveen.kishor123@gmail.com>
2017-03-06 02:26:51 +0530

    Create UnaryDemo.java

---
+++ UnaryDemo.java
@@ -0,0 +1,19 @@
+
+public class UnaryDemo {
+	
+	public static void main(String[] args) {
+		
+		int x = 10;
+		int y = 20;
+		
+		System.out.println(++x); // 11
+		
+		System.out.println(x++); // 11
+		
+		System.out.println(x);   // 12
+		
+		
+				
+	}
+
+}
