d13aa330af3ca3da2b091e8227979e48aebd7981
Praveen Kishor <praveen.kishor123@gmail.com>
2017-03-06 02:39:05 +0530

    Create Main9.java

---
+++ Main9.java
@@ -0,0 +1,21 @@
+
+/**
+ * @author praveen kishor
+ *
+ */
+public class Main9 {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+		Double dObj = new Double(10.25);
+		
+		// Using toString method of Double class to 
+		// convert Double to String
+		String str = dObj.toString();
+		
+		System.out.println(str); // 10.25
+		
+	}
+
+}
