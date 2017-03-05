0391a31ef0c0058fc592fd730782692ed59f0d72
Praveen Kishor <praveen.kishor123@gmail.com>
2017-03-06 02:35:12 +0530

    Create Main8.java

---
+++ Main8.java
@@ -0,0 +1,20 @@
+
+class Main {
+	
+	public static void main(String[] args) {
+		
+		double d = 10.56;
+		
+		System.out.println(d); // 10.56
+		
+		// use double constructor to convert
+		// double primitive type to Double Object
+		Double dObj = new Double(d);
+		
+		System.out.println(dObj); // 10.56
+		
+		
+		
+	}
+
+}
